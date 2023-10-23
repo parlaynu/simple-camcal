7037dad904a837f67fdbf549fb51d1d29239112c
Paul Ryan <g5muzu8u@duck.com>
2023-10-24 07:33:44 +1100

    fix argparse options

--- viewer.py
+++ viewer.py
@@ -128,8 +128,7 @@ def main():
     parser.add_argument('--vflip', help='vertical flip', action='store_true')
     parser.add_argument('--mode', 
                             choices=['2', '3', '4', '5'],
-                            default=None, const='2',
-                            nargs='?',
+                            default=2,
                             help='the camera mode (default: 2)', 
                         )
     parser.add_argument('calconfig', help='calibration config file', type=str, nargs='?', default=None)
